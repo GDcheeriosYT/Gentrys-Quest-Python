8c567a657db27d7c733593280b0eb2530b495c0c
GDcheerios <54640414+GDcheeriosYT@users.noreply.github.com>
2022-09-08 19:00:13 +0000

    replit example

---
+++ main.py
@@ -0,0 +1,12 @@
+#list of colors https://rich.readthedocs.io/en/stable/appendix/colors.html
+
+'''
+brayden: hi
+carter: hi
+'''
+
+from rich.console import Console
+
+print("[blue]CARTER [orange]BRAYDEN")
+console = Console()
+console.print("[blue underline]CARTER[white on red] [orange1]BRAYDEN")

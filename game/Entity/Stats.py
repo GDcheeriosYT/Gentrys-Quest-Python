2c8fe246f3df4890b38c6711467d95019a605d89
GDcheerios <minecreeper0913@gmail.com>
2022-09-23 12:20:41 -0500

    updated Stats.py

--- game/Entity/Stats.py
+++ game/Entity/Stats.py
@@ -1,11 +1,14 @@
 from enum import Enum
 class Stats(Enum):
-    Strength = 0
-    Defense = 1
-    Magic = 2
-    Magic_Defense = 3
-    Arcane = 4
-    Health = 5
-    Dexterity = 6
+    Health = 0
+    Attack = 1
+    Defense = 2
+    CritRate = 3
+    CritDamage = 4
+    Strength = 5
+    Magic = 6
+    Magic_Defense = 7
+    Arcane = 8
+    Dexterity = 9
 
 

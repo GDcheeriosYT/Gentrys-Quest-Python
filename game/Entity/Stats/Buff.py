09eb1da3fe685e7bcfdb528010dbb8cc398e8613
GDcheerios <minecreeper0913@gmail.com>
2022-10-11 02:28:36 -0500

    added test method in Buff.py
    needs:
    - a setting for the attribute type

--- game/Entity/Stats/Buff.py
+++ game/Entity/Stats/Buff.py
@@ -3,6 +3,19 @@
 from .StatTypes import StatTypes
 from .Experience import Experience
 
+# config packages
+from Config.NumberSetting import NumberSetting
+from Config.StringSetting import StringSetting
+from Config.ToggleSetting import ToggleSetting
+from Config.ClassSetting import ClassSetting
+from Config.SettingManager import SettingManager
+
+# IO packages
+from IO import Window
+
+# graphics packages
+from Graphics.Text.Text import Text
+
 # built-in packages
 import random
 
@@ -34,6 +47,18 @@ class Buff:
             self.attribute_type = attribute_type
         self.experience = experience
         self.is_percent = is_percent
+        self.settings = [
+            NumberSetting("level", self.experience.level, 1),
+            ToggleSetting("is percent type", self.is_percent)
+        ]
 
     def __repr__(self):
-        return f"{self.attribute_type.name} 0{'%' if self.is_percent else ''}"
+        return f"{self.attribute_type.name} 0{'%' if self.is_percent else ''}[{self.experience.level}]"
+
+    def test(self):
+        Window.clear()
+        Text(self.__repr__()).display()
+        self.settings = SettingManager(self.settings).config_settings(True)
+        self.experience.level = self.settings[0].value
+        self.is_percent = self.settings[1].toggled
+        return Text(self)

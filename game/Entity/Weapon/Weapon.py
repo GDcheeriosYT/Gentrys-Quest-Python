107f98bfd4d37facdb57b47e99089ca7cc0d021a
carterballard123 <112200106+carterballard123@users.noreply.github.com>
2022-10-18 13:34:52 -0500

    FINISHED WEAPION TEAST WEAPON METHOF WHY BRAYDEN IS SO SPELLING

--- game/Entity/Weapon/Weapon.py
+++ game/Entity/Weapon/Weapon.py
@@ -6,6 +6,18 @@ from ..Stats.Experience import Experience
 from ..Stats.StatTypes import StatTypes
 from ..Stats.Buff import Buff
 
+# Config packages
+from Config.NumberSetting import NumberSetting
+from Config.StringSetting import StringSetting
+from Config.ToggleSetting import ToggleSetting
+from Config.ClassSetting import ClassSetting
+from Config.SettingManager import SettingManager
+
+# Graphics packages
+from Graphics.Text.Text import Text
+
+# IO packages
+from IO import Window
 
 class Weapon(Entity):
     """
@@ -54,6 +66,16 @@ class Weapon(Entity):
         self.attack = attack
         self.buff = buff
         self.verbs = verbs
+        self.settings = [
+            StringSetting("name", self.weapon.name),
+            StringSetting("description", self.weapon.description),
+            StringSetting("weapon_type", self.weapon.weapon_type),
+            NumberSetting("attack", self.weapon.attack),
+            ClassSetting("buff", self.weapon.buff),
+            ClassSetting("verbs", self.weapon.verbs),
+            NumberSetting("star_rating", self.weapon.star_rating.value, 1, 5),
+            NumberSetting("experience", self.weapon.experience.level, 1)
+        ]
 
     def __repr__(self):
         return (
@@ -64,3 +86,17 @@ base attack: {self.attack}
 attribute: {self.buff}
 """
         )
+
+    def test(self):
+        Window.clear()
+        Text(self.weapon).display()
+        self.settings = SettingManager(self.settings).config_settings(False)
+        self.weapon.name = self.settings[0].text
+        self.weapon.description = self.settings[1].text
+        self.weapon.weapon_type = self.settings[2].text
+        self.weapon.attack = self.settings[3].value
+        self.weapon.buff = self.settings[4].instance_class
+        self.weapon.verbs = self.settings[5].instance_class
+        self.weapon.star_rating = StarRating(self.settings[6].value)
+        self.weapon.experience.level = self.settings[7].value
+        return self.settings

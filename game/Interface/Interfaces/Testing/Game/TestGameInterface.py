7f13a0cd62734cfde36546a5bdd86901a82e0af1
GDcheerios <minecreeper0913@gmail.com>
2022-11-16 21:30:11 -0600

    put test game in while loop

--- game/Interface/Interfaces/Testing/Game/TestGameInterface.py
+++ game/Interface/Interfaces/Testing/Game/TestGameInterface.py
@@ -17,15 +17,19 @@ class TestGameInterface:
         self.game = Game(GameData(None))
 
     def start(self):
-        choice = get_int("1. play\n"
-                         "2. edit data\n"
-                         "3. quit")
+        while True:
+            choice = get_int("1. play\n"
+                             "2. edit data\n"
+                             "3. quit")
 
-        if choice == 1:
-            self.game.start()
+            if choice == 1:
+                self.game.start()
 
-        elif choice == 2:
-            test_inventory_interface = InventoryTestInterface()
-            test_inventory_interface.inventory = self.game.game_data.inventory
-            test_inventory_interface.__repr__()
-            self.game.game_data.inventory = test_inventory_interface.inventory
+            elif choice == 2:
+                test_inventory_interface = InventoryTestInterface()
+                test_inventory_interface.inventory = self.game.game_data.inventory
+                test_inventory_interface.__repr__()
+                self.game.game_data.inventory = test_inventory_interface.inventory
+
+            else:
+                break

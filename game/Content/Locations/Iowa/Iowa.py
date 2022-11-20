02fe6e8ad597e1ffbc84e82004d3c2624ac94384
GDcheerios <minecreeper0913@gmail.com>
2022-11-20 01:15:04 -0600

    Reimplemented ValleyHighSchool into Iowa

--- game/Content/Locations/Iowa/Iowa.py
+++ game/Content/Locations/Iowa/Iowa.py
@@ -8,6 +8,7 @@ from .CartersHouse.CartersHouse import CartersHouse
 from .UnifinishedConstructionSite.UnfinishedConstructionSite import UnfinishedConstructionSite
 from .DownTown.DownTown import DownTown
 from .Target.Target import Target
+from .ValleyHighSchool.ValleyHighSchool import ValleyHighSchool
 
 
 class Iowa(Location):
@@ -19,6 +20,7 @@ class Iowa(Location):
                 CartersHouse(),
                 UnfinishedConstructionSite(),
                 DownTown(),
-                Target()
+                Target(),
+                ValleyHighSchool()
             ]
         )

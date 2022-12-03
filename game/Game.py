508589bb039afe52f5764ef2dd45b015583fb382
GDcheerios <minecreeper0913@gmail.com>
2022-12-03 02:22:05 -0600

    added while loops for more convenient traversal through the menus

--- game/Game.py
+++ game/Game.py
@@ -90,47 +90,56 @@ class Game:
                 choices = get_int("Main Menu\n"
                                   "1. Play\n"
                                   "2. Settings\n"
-                                  "3. Changelog\n"
-                                  "4. Quit")
+                                  "3. Quit")
+
                 if choices == 1:
                     choices1 = get_int("1. Singleplayer\n"
-                                       "2. Multiplayer\n"
-                                       "3. Back")
+                                       "2. Back")
+
                     if choices1 == 1:
-                        choices2 = get_int("1. Travel\n"
-                                           "2. Gacha\n"
-                                           "3. Inventory\n"
-                                           "4. Back")
+                        while True:
+                            choices2 = get_int("1. Travel\n"
+                                               "2. Gacha\n"
+                                               "3. Inventory\n"
+                                               "4. Back")
 
-                        if choices2 == 1:
-                            iowa = Iowa()
-                            choices3 = get_int("1. Iowa\n"
-                                               "2. back")
+                            if choices2 == 1:
+                                iowa = Iowa()
+                                while True:
+                                    choices3 = get_int("1. Iowa\n"
+                                                       "2. back")
 
-                            if choices3 == 1:
-                                iowa.list_areas()
-                                iowa.select_area(self.equipped_character, self.game_data.inventory)
+                                    if choices3 == 1:
+                                        iowa.list_areas()
+                                        iowa.select_area(self.equipped_character, self.game_data.inventory)
 
-                        elif choices2 == 2:
-                            valley_high_school = ValleyHighSchool()
-                            base_gacha = BaseGacha()
-                            Text(f"1. {valley_high_school.name.raw_output()}\n"
-                                 f"2. {base_gacha.name.raw_output()}").display()
-                            choices3 = get_int("3. back")
+                                    else:
+                                        break
 
-                            if choices3 == 1:
-                                valley_high_school.manage_input(self.game_data.inventory)
+                            elif choices2 == 2:
+                                while True:
+                                    valley_high_school = ValleyHighSchool()
+                                    base_gacha = BaseGacha()
+                                    Text(f"1. {valley_high_school.name.raw_output()}\n"
+                                         f"2. {base_gacha.name.raw_output()}").display()
+                                    choices3 = get_int("3. back")
 
-                            elif choices3 == 2:
-                                base_gacha.manage_input(self.game_data.inventory)
+                                    if choices3 == 1:
+                                        valley_high_school.manage_input(self.game_data.inventory)
 
-                        elif choices2 == 3:
-                            inventory_results = self.game_data.inventory.manage_input(self.equipped_character)
-                            if inventory_results is not None:
-                                self.equipped_character = inventory_results
+                                    elif choices3 == 2:
+                                        base_gacha.manage_input(self.game_data.inventory)
 
-                    elif choices1 == 2:
-                        InfoText("Coming Soon...").display(enter_prompt=True)
+                                    else:
+                                        break
+
+                            elif choices2 == 3:
+                                inventory_results = self.game_data.inventory.manage_input(self.equipped_character)
+                                if inventory_results is not None:
+                                    self.equipped_character = inventory_results
+
+                            else:
+                                break
 
                 elif choices == 2:
                     Window.clear()
@@ -139,7 +148,7 @@ class Game:
                     except TypeError:
                         Window.clear()
 
-                elif choices == 4:
+                elif choices == 3:
                     in_game = False
             except ValueError:
                 WarningText("Number please...").display()

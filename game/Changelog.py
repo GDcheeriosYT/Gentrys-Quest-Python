127bf07ffcb7d5934b514a31a66e56a9c3145be9
GDcheerios <minecreeper0913@gmail.com>
2022-12-12 09:32:33 -0600

    added links to changelog

--- game/Changelog.py
+++ game/Changelog.py
@@ -10,46 +10,46 @@ from IO import Window
 
 # change log groupings
 gameplay = [
-    InfoText("Added Changelog!")
+    InfoText("Added Changelog! [link=https://github.com/GDcheeriosYT/Gentrys-Quest-Python/pull/35]PR\[#35][/link]")
 ]
 graphics = []
 content = []
 online = []
 code_structure = []
 testing = [
-    InfoText("Added Changelog View Testing")
+    InfoText("Added Changelog View Testing! [link=https://github.com/GDcheeriosYT/Gentrys-Quest-Python/pull/35]PR\[#35][/link]")
 ]
 
 
 def display_changelog():
     Window.clear()
     if len(gameplay) > 0:
-        Window.place_rule("Gameplay")
+        #Window.place_rule("Gameplay")
         for gameplay_change in gameplay:
             gameplay_change.display()
 
     if len(graphics) > 0:
-        Window.place_rule("Graphics")
+        #Window.place_rule("Graphics")
         for graphics_change in graphics:
             graphics_change.display()
 
     if len(content) > 0:
-        Window.place_rule("Content")
+        #Window.place_rule("Content")
         for content_change in content:
             content_change.display()
 
     if len(online) > 0:
-        Window.place_rule("Online")
+        #Window.place_rule("Online")
         for online_change in online:
             online_change.display()
 
     if len(code_structure) > 0:
-        Window.place_rule("Code Structure")
+        #Window.place_rule("Code Structure")
         for code_structure_change in code_structure:
             code_structure_change.display()
 
     if len(testing) > 0:
-        Window.place_rule("Testing")
+        #Window.place_rule("Testing")
         for testing_change in testing:
             testing_change.display()
 

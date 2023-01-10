3f8ef672a3b46eabe72caf7cab3c46b0bdc0e00e
GDcheerios <minecreeper0913@gmail.com>
2023-01-10 13:03:17 -0600

    added changelog category rules

--- game/Changelog.py
+++ game/Changelog.py
@@ -21,7 +21,8 @@ gameplay = [
 graphics = []
 content = [
     InfoText("Added Nigeria location! [link=https://github.com/GDcheeriosYT/Gentrys-Quest-Python/pull/41]PR\[#41][/link] by GDcheerios"),
-    InfoText("Added content to Brayden's House! by GDcheerios")
+    InfoText("Added content to Brayden's House! by GDcheerios"),
+    InfoText("Added Gentry's House location! by GDcheerios")
 ]
 online = [
     InfoText("Added online features! [link=https://github.com/GDcheeriosYT/Gentrys-Quest-Python/pull/49]PR\[#49][/link] by GDcheerios")
@@ -38,32 +39,32 @@ def display_changelog(version: str):
     Window.clear()
     Window.place_rule(f"Changelog {version}")
     if len(gameplay) > 0:
-        #Window.place_rule("Gameplay")
+        Window.place_rule("Gameplay")
         for gameplay_change in gameplay:
             gameplay_change.display()
 
     if len(graphics) > 0:
-        #Window.place_rule("Graphics")
+        Window.place_rule("Graphics")
         for graphics_change in graphics:
             graphics_change.display()
 
     if len(content) > 0:
-        #Window.place_rule("Content")
+        Window.place_rule("Content")
         for content_change in content:
             content_change.display()
 
     if len(online) > 0:
-        #Window.place_rule("Online")
+        Window.place_rule("Online")
         for online_change in online:
             online_change.display()
 
     if len(code_structure) > 0:
-        #Window.place_rule("Code Structure")
+        Window.place_rule("Code Structure")
         for code_structure_change in code_structure:
             code_structure_change.display()
 
     if len(testing) > 0:
-        #Window.place_rule("Testing")
+        Window.place_rule("Testing")
         for testing_change in testing:
             testing_change.display()
 
